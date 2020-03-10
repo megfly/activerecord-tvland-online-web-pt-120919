a7defb304b550d62b3ba655c774bc989a9a79e1d
Meghan Flynn <mflynn333@gmail.com>
2020-03-10 22:03:31 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+
+  belongs_to :network
+  has_many :characters
+
+  def actors_list
+    self.characters.map do |c|
+      "#{c.actor.full_name}"
+      end
+    end
+      
 end
